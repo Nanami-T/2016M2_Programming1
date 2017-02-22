4414d438f46e6e51e8b5b53135c8387719e3bb98
Tokaji_Nanami_e5428 <e5428@gm.kochi-ct.jp>
2017-02-22 11:51:55 +0900

    Create kudari.c

---
+++ kudari.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+int i, j, k;
+
+printf("i = ");
+ scanf("%d",&i);
+ 
+for( j = 1; j <= i, j ++ ){
+  for( k = 1, k <= j,; k ++ ) printf("*");
+ printf("\n");
+}
+
+return 0;
+
+}
